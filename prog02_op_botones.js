7beb93f5a4570e386179383c7ac854f6f1f55f0e
A01769848 <123998505+A01769848@users.noreply.github.com>
2023-03-08 15:11:28 -0600

    Add files via upload

---
+++ prog02_op_botones.js
@@ -0,0 +1,47 @@
+function suma(){
+	x=parseInt(document.datos.v1.value);
+	y=parseInt(document.datos.v2.value);
+	r=x+y;
+	document.datos.opera.value="Suma";
+	document.datos.res.value=r;
+}
+
+function resta(){
+	x=parseInt(document.datos.v1.value);
+	y=parseInt(document.datos.v2.value);
+	r=x-y;
+	document.datos.opera.value="Resta";
+	document.datos.res.value=r;
+}
+
+function mult(){
+	x=parseInt(document.datos.v1.value);
+	y=parseInt(document.datos.v2.value);
+	r=x*y;
+	document.datos.opera.value="Multiplicación";
+	document.datos.res.value=r;
+}
+
+function div(){
+	x=parseInt(document.datos.v1.value);
+	y=parseInt(document.datos.v2.value);
+	r=x/y;
+	document.datos.opera.value="División";
+	document.datos.res.value=r;
+}
+
+function ejecutar(){
+	op=document.datos.op.value;
+	if (op=="1"){
+		suma();
+	}
+	if (op=="2"){
+		resta();
+	}
+	if (op=="3"){
+		mult();
+	}
+	if (op=="4"){
+		div();
+	}
+}
